b739ec3b3b0d5f819d875255235242bd429eb87a
ELAIASSI Samy <samy.elaiassi@3ds.com>
2021-07-31 10:47:20 +0200

    backend init project

---
+++ src/main/java/com/example/animewatcherbackend/AnimeWatcherBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.animewatcherbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeWatcherBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeWatcherBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/animewatcherbackend/AnimeWatcherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animewatcherbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeWatcherBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
