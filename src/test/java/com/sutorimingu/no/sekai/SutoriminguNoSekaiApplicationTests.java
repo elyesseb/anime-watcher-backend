f59ea0dc78c5cabdb7c936e4d06b3a1abdad5b35
ufhopla <elyesse.benhadj@gmail.com>
2022-10-23 19:08:14 +0200

    Prepare to deploy

--- src/test/java/com/sutorimingu/no/sekai/SutoriminguNoSekaiApplicationTests.java
+++ src/test/java/com/sutorimingu/no/sekai/SutoriminguNoSekaiApplicationTests.java
@@ -1,13 +1,12 @@
 package com.sutorimingu.no.sekai;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SutoriminguNoSekaiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
