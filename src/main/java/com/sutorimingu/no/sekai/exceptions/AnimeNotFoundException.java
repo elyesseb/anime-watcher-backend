0357b5b467c68100e6bbcccc582ae1d08aba73af
ufhopla <elyesse.benhadj@gmail.com>
2022-11-03 16:49:33 +0100

    Fix episodeDto, and some security

--- src/main/java/com/sutorimingu/no/sekai/exceptions/AnimeNotFoundException.java
+++ src/main/java/com/sutorimingu/no/sekai/exceptions/AnimeNotFoundException.java
@@ -1,20 +1,12 @@
 package com.sutorimingu.no.sekai.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-
-
 public class AnimeNotFoundException extends RuntimeException {
 
     public AnimeNotFoundException(Long id) {
-        throw new ResponseStatusException(
-                HttpStatus.NOT_FOUND, "Could not find anime" + id
-        );
+        super("Could not find anime " + id);
     }
 
     public AnimeNotFoundException(String name) {
-        throw new ResponseStatusException(
-                HttpStatus.NOT_FOUND, "Could not find anime" + name
-        );
+        super("Could not find anime " + name);
     }
 }
